61e716b0e36b73453dffc528c6149d9f3c6821a9
Alexander R. Torrijos <artorrijos@gmail.com>
2020-07-09 15:18:08 +0800

    Initial commit

---
+++ includes/class-pcci-fullcalendar-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://stormwild.github.io/
+ * @since      1.0.0
+ *
+ * @package    Pcci_Fullcalendar
+ * @subpackage Pcci_Fullcalendar/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Pcci_Fullcalendar
+ * @subpackage Pcci_Fullcalendar/includes
+ * @author     Alexander Torrijos <artorrijos@gmail.com>
+ */
+class Pcci_Fullcalendar_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-pcci-fullcalendar-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://stormwild.github.io/
+ * @since      1.0.0
+ *
+ * @package    Pcci_Fullcalendar
+ * @subpackage Pcci_Fullcalendar/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Pcci_Fullcalendar
+ * @subpackage Pcci_Fullcalendar/includes
+ * @author     Alexander Torrijos <artorrijos@gmail.com>
+ */
+class Pcci_Fullcalendar_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
